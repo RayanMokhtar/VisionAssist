05c4d592b08ad18463d8774ffe81a378a71cea42
lazibadam <lazibadam@gmail.com>
2026-04-01 01:23:34 +0200

    ajout de harris et points interets

--- src/OpticalFlow.cpp
+++ src/OpticalFlow.cpp
@@ -17,7 +17,7 @@ struct vectDepl
 
 void niveauGris(Mat frame, Mat *frameGris)
 {
-    *frameGris = cv::Mat(frame.rows, frame.cols, CV_8UC1, cv::Scalar(0));
+    *frameGris = cv::Mat(frame.rows, frame.cols, CV_32FC1, cv::Scalar(0));
 
     for (int y = 0; y < frame.rows; y++)
     {
@@ -25,52 +25,222 @@ void niveauGris(Mat frame, Mat *frameGris)
         {
             cv::Vec3b pixel = frame.at<cv::Vec3b>(y, x);
 
-            (*frameGris).at<uchar>(y, x) = (pixel[0] + pixel[1] + pixel[2]) / 3;
+            (*frameGris).at<float>(y, x) = (pixel[0] + pixel[1] + pixel[2]) / 3;
         }
     }
 }
 
-void sobelX(Mat frame, Mat *frameSobelX)
+void sobelX(Mat *frame, Mat *frameSobelX)
 {
-    *frameSobelX = cv::Mat(frame.rows, frame.cols, CV_8UC1, cv::Scalar(0));
+    *frameSobelX = cv::Mat(frame->rows, frame->cols, CV_32FC1, cv::Scalar(0));
     float somme;
 
-    for (int y = 1; y < frame.rows-1; y++)
+    for (int y = 1; y < frame->rows-1; y++)
     {
-        for (int x = 1; x < frame.cols-1; x++)
+        for (int x = 1; x < frame->cols-1; x++)
         {
-            somme = frame.at<uchar>(y-1, x-1)*-1 + frame.at<uchar>(y-1, x+1) + frame.at<uchar>(y, x-1)*-2 +
-                    frame.at<uchar>(y, x+1)*2 + frame.at<uchar>(y+1, x-1)*-1 + frame.at<uchar>(y+1, x+1);
-            (*frameSobelX).at<uchar>(y, x) = somme / 4;
+            somme = frame->at<float>(y-1, x-1)*-1 + frame->at<float>(y-1, x+1) + frame->at<float>(y, x-1)*-2 +
+                    frame->at<float>(y, x+1)*2 + frame->at<float>(y+1, x-1)*-1 + frame->at<float>(y+1, x+1);
+            (*frameSobelX).at<float>(y, x) = somme / 4;
         }
     }
 }
 
-void sobelY(Mat frame, Mat *frameSobelY)
+void sobelY(Mat *frame, Mat *frameSobelY)
 {
-    *frameSobelY = cv::Mat(frame.rows, frame.cols, CV_8UC1, cv::Scalar(0));
+    *frameSobelY = cv::Mat(frame->rows, frame->cols, CV_32FC1, cv::Scalar(0));
     float somme;
 
-    for (int y = 1; y < frame.rows-1; y++)
+    for (int y = 1; y < frame->rows-1; y++)
     {
-        for (int x = 1; x < frame.cols-1; x++)
+        for (int x = 1; x < frame->cols-1; x++)
         {
-            somme = frame.at<uchar>(y-1, x-1)*-1 + frame.at<uchar>(y-1, x)*-2 + frame.at<uchar>(y-1, x+1)*-1 +
-                    frame.at<uchar>(y+1, x-1) + frame.at<uchar>(y+1, x)*2 + frame.at<uchar>(y+1, x+1);
-            (*frameSobelY).at<uchar>(y, x) = somme / 4;
+            somme = frame->at<float>(y-1, x-1)*-1 + frame->at<float>(y-1, x)*-2 + frame->at<float>(y-1, x+1)*-1 +
+                    frame->at<float>(y+1, x-1) + frame->at<float>(y+1, x)*2 + frame->at<float>(y+1, x+1);
+            (*frameSobelY).at<float>(y, x) = somme / 4;
         }
     }
 }
 
 void diffIntensite(Mat frame1, Mat frame2, Mat *frameDiffIntensite)
 {
-    *frameDiffIntensite = cv::Mat(frame1.rows, frame2.cols, CV_8UC1, cv::Scalar(0));
+    *frameDiffIntensite = cv::Mat(frame1.rows, frame2.cols, CV_32FC1, cv::Scalar(0));
 
     for (int y = 0; y < frame1.rows; y++)
     {
         for (int x = 0; x < frame2.cols; x++)
         {
-            (*frameDiffIntensite).at<uchar>(y, x) = frame1.at<uchar>(y, x) - frame2.at<uchar>(y, x);
+            (*frameDiffIntensite).at<float>(y, x) = frame1.at<float>(y, x) - frame2.at<float>(y, x);
+        }
+    }
+}
+
+void carre(Mat *frame, Mat *frameCarre)
+{
+    *frameCarre = cv::Mat(frame->rows, frame->cols, CV_32FC1, cv::Scalar(0));
+
+    for (int y = 0; y < frame->rows; y++)
+    {
+        for (int x = 0; x < frame->cols; x++)
+        {
+            (*frameCarre).at<float>(y, x) = frame->at<float>(y, x) * frame->at<float>(y, x);
+        }
+    }
+}
+
+void masqueGaussien(Mat *masque, float sigma, int taille)
+{
+    *masque = cv::Mat(taille, taille, CV_32FC1, cv::Scalar(0));
+    int idx = taille / 2;
+   
+    for (int y = -idx; y <= idx; y++)
+    {
+        for (int x = -idx; x <= idx; x++)
+        {
+            (*masque).at<float>(y + idx, x + idx) = (1 / (2*M_PI*(sigma*sigma))) * std::exp(-((y*y + x*x) / (2*(sigma*sigma))));
+        }
+    }
+}
+
+void filtreGaussien(Mat frame, Mat *frameGaussien, Mat masque)
+{
+    *frameGaussien = cv::Mat(frame.rows, frame.cols, CV_32FC1, cv::Scalar(0));
+    float somme;
+
+    for (int y = 1; y < frame.rows-1; y++)
+    {
+        for (int x = 1; x < frame.cols-1; x++)
+        {
+            somme = 0;
+            for (int i = 0; i < masque.rows; i++)
+            {
+                for (int j = 0; j < masque.cols; j++)
+                {
+                    somme += masque.at<float>(i, j)*frame.at<float>(y, x);
+                }
+            }
+            (*frameGaussien).at<float>(y, x) = somme ;
+        }
+    }
+}
+
+void somme(Mat frame1, Mat frame2, Mat *frameSomme)
+{
+    *frameSomme = cv::Mat(frame1.rows, frame1.cols, CV_32FC1, cv::Scalar(0));
+
+    for (int y = 0; y < frame2.rows; y++)
+    {
+        for (int x = 0; x < frame2.cols; x++)
+        {
+            (*frameSomme).at<float>(y, x) = frame1.at<float>(y, x) + frame2.at<float>(y, x);
+        }
+    }
+}
+
+void produit(Mat *frame1, Mat *frame2, Mat *frameProduit)
+{
+    *frameProduit = cv::Mat(frame1->rows, frame1->cols, CV_32FC1, cv::Scalar(0));
+
+    for (int y = 0; y < frame2->rows; y++)
+    {
+        for (int x = 0; x < frame2->cols; x++)
+        {
+            (*frameProduit).at<float>(y, x) = frame1->at<float>(y, x) * frame2->at<float>(y, x);
+        }
+    }
+}
+
+void formuleHarris(Mat frame1, Mat frame2, Mat frame3, Mat *frameHarris, float lambda)
+{
+    *frameHarris = cv::Mat(frame1.rows, frame1.cols, CV_32FC1, cv::Scalar(0));
+
+    for (int y = 0; y < frame2.rows; y++)
+    {
+        for (int x = 0; x < frame2.cols; x++)
+        {
+            (*frameHarris).at<float>(y, x) = frame1.at<float>(y, x) - frame2.at<float>(y, x) - lambda*frame3.at<float>(y, x);
+        }
+    }
+}
+
+void harris(Mat frame, Mat *frameGris, Mat *frameSobelX, Mat *frameSobelY, Mat *frameHarris, Mat masque, float lambda)
+{
+    Mat frameSobelCarreX;
+    Mat frameSobelCarreY;
+    Mat frameSobelCarreGaussienX;
+    Mat frameSobelCarreGaussienY;
+    Mat frameSobelProduitXY;
+    Mat frameSobelCarreGaussienProduitXY;
+    Mat frameSobelProduitGaussienXY;
+    Mat frameSobelProduitGaussienXYCarre;
+    Mat frameSobelCarreGaussienSommeXY;
+    Mat frameSobelCarreGaussienSommeCarreXY;
+
+    niveauGris(frame, frameGris);
+
+    sobelX(frameGris, frameSobelX);
+    sobelY(frameGris, frameSobelY);
+
+    carre(frameSobelX, &frameSobelCarreX);
+    carre(frameSobelY, &frameSobelCarreY);
+
+    filtreGaussien(frameSobelCarreX, &frameSobelCarreGaussienX, masque);
+    filtreGaussien(frameSobelCarreY, &frameSobelCarreGaussienY, masque);
+
+    produit(frameSobelX, frameSobelY, &frameSobelProduitXY);
+    produit(&frameSobelCarreGaussienX, &frameSobelCarreGaussienY, &frameSobelCarreGaussienProduitXY);
+
+    filtreGaussien(frameSobelProduitXY, &frameSobelProduitGaussienXY, masque);
+
+    carre(&frameSobelProduitGaussienXY, &frameSobelProduitGaussienXYCarre);
+
+    somme(frameSobelCarreGaussienX, frameSobelCarreGaussienY, &frameSobelCarreGaussienSommeXY);
+
+    carre(&frameSobelCarreGaussienSommeXY, &frameSobelCarreGaussienSommeCarreXY);
+
+    formuleHarris(frameSobelCarreGaussienProduitXY, frameSobelProduitGaussienXYCarre, frameSobelCarreGaussienSommeCarreXY, frameHarris, lambda);
+}
+
+std::vector<cv::Point> trouverPointsInteret(Mat frameHarris, Mat &frameCouleur)
+{
+    std::vector<cv::Point> pointInterets;
+
+    for (int y = 1; y < frameHarris.rows - 1; y++)
+    {
+        for (int x = 1; x < frameHarris.cols - 1; x++)
+        {
+            float val = frameHarris.at<float>(y, x);
+
+            if (
+                val > frameHarris.at<float>(y-1, x-1) &&
+                val > frameHarris.at<float>(y-1, x) &&
+                val > frameHarris.at<float>(y-1, x+1) &&
+                val > frameHarris.at<float>(y, x-1) &&
+                val > frameHarris.at<float>(y, x+1) &&
+                val > frameHarris.at<float>(y+1, x-1) &&
+                val > frameHarris.at<float>(y+1, x) &&
+                val > frameHarris.at<float>(y+1, x+1))
+            {
+                circle(frameCouleur, Point(x, y), 3, Scalar(0, 0, 255), 1);
+                pointInterets.push_back(Point(x, y));
+            }
+        }
+    }
+
+    return pointInterets;
+}
+
+void normalisation(Mat frame, Mat *frameNormaliser)
+{
+    double min, max;
+   
+    cv::minMaxLoc(frame, &min, &max);
+   
+    for (int y = 0; y < frame.rows; y++)
+    {
+        for (int x = 0; x < frame.cols; x++)
+        {
+            (*frameNormaliser).at<float>(y, x) = ((frame.at<float>(y, x) - min) / (max - min)) * 255;
         }
     }
 }
@@ -78,7 +248,7 @@ void diffIntensite(Mat frame1, Mat frame2, Mat *frameDiffIntensite)
 int main(int argc, char** argv)
 {
     Mat frame;
-    Mat frameGris, frameSobelX, frameSobelY, frameDiffIntensite, frameOld, frameGrisOld;
+    Mat frameGris, frameSobelX, frameSobelY, frameDiffIntensite, frameOld, frameGrisOld, frameHarris, masque, frameGrisFloat;
 
     VideoCapture cap(0);
 
@@ -88,24 +258,79 @@ int main(int argc, char** argv)
     }
 
     cap >> frameOld;
-    niveauGris(frameOld, &frameGrisOld);
+
+    masqueGaussien(&masque, 0.04, 3);
+
+/*
+    for(;;)
+    {
+        cap >> frame;
+
+        Size newSize(620, 480);
+        
+        resize(frame, frame, newSize, 0, 0, INTER_LINEAR);
+
+        niveauGris(frame, &frameGris);
+        sobelX(frameGris, &frameSobelX);
+        sobelY(frameGris, &frameSobelY);
+        niveauGris(frameOld, &frameGrisOld);
+        diffIntensite(frameGris, frameGrisOld, &frameDiffIntensite);
+
+        harris(frameGris, &frameHarris, masque, 1.0);
+//cornerHarris(frameGris, frameHarris, 1, 3, 0.04);
+//normalize(frameHarris, frameHarris, 0, 255, NORM_MINMAX, CV_8UC1);
+        normalisation(frameHarris, &frameHarris);
+        //trouverPointsInteret(frameHarris, frame);
+
+        //frameGris.convertTo(frameGris, CV_8UC1);
+        //frameSobelX.convertTo(frameSobelX, CV_8UC1);
+        //frameSobelY.convertTo(frameSobelY, CV_8UC1);
+        //frameDiffIntensite.convertTo(frameDiffIntensite, CV_8UC1);
+        frameHarris.convertTo(frameHarris, CV_8UC1);
+
+        imshow("capture", frame);
+        //imshow("gris", frameGris);
+        //imshow("sobel x", frameSobelX);
+        //imshow("sobel y", frameSobelY);
+        //imshow("difference", frameDiffIntensite);
+        imshow("harris", frameHarris);
+
+        frame.copyTo(frameOld);
+
+        if(waitKey(33) == 27) break;
+    }
+*/
 
     for(;;)
     {
         cap >> frame;
 
-        niveauGris(frame, &frameGris);
-        sobelX(frameGris, &frameSobelX);
-        sobelY(frameGris, &frameSobelY);
+        Size newSize(620, 480);
+        
+        resize(frame, frame, newSize, 0, 0, INTER_LINEAR);
+
+        niveauGris(frameOld, &frameGrisOld);
+
+        harris(frame, &frameGris, &frameSobelX, &frameSobelY, &frameHarris, masque, 1.0);
+        normalisation(frameHarris, &frameHarris);
+        trouverPointsInteret(frameHarris, frame);
+
         diffIntensite(frameGris, frameGrisOld, &frameDiffIntensite);
 
-        imshow("capture", frame);
-        imshow("gris", frameGris);
-        imshow("sobel x", frameSobelX);
-        imshow("sobel y", frameSobelY);
-        imshow("difference", frameDiffIntensite);
+        //frameGris.convertTo(frameGris, CV_8UC1);
+        //frameSobelX.convertTo(frameSobelX, CV_8UC1);
+        //frameSobelY.convertTo(frameSobelY, CV_8UC1);
+        //frameDiffIntensite.convertTo(frameDiffIntensite, CV_8UC1);
+        frameHarris.convertTo(frameHarris, CV_8UC1);
 
-        frameGris.copyTo(frameGrisOld);
+        imshow("capture", frame);
+        //imshow("gris", frameGris);
+        //imshow("sobel x", frameSobelX);
+        //imshow("sobel y", frameSobelY);
+        //imshow("difference", frameDiffIntensite);
+        imshow("harris", frameHarris);
+
+        frame.copyTo(frameOld);
 
         if(waitKey(33) == 27) break;
     }
